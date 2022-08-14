f7d5f15b5f54615024cba8d23100576beff91906
OmgCopito95 <july1995bolt@gmail.com>
2022-08-14 17:57:32 -0300

    borra campo

--- src/database/models/usuario.js
+++ src/database/models/usuario.js
@@ -23,10 +23,6 @@ module.exports = (sequelize, DataTypes) => {
     edad: {
       type: DataTypes.INTEGER,
     },
-    profesion: {
-      type: DataTypes.STRING,
-      allowNull: true
-    },
     createdAt: { // fecha de creacion
       type: DataTypes.DATE, // tipo de dato
       field: 'created_at', // nombre de la columna
